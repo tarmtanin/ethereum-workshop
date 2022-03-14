06450d34e66d1dc0fe9ed72c5ca1ddbb25a06cfb
Tanin <tanin.tantisirawattanakul@iamconsulting.co.th>
2022-03-14 15:45:22 +0700

    Update bank

--- Contract/BankWorkshop.sol
+++ Contract/BankWorkshop.sol
@@ -3,10 +3,23 @@ pragma solidity ^0.8.10;
 
 contract Bank {
     // Declare accounts parameter
+    mapping(address => uint256) private accounts;
 
     // Get balance
+    function balance() public view returns (uint256) {
+        return accounts[msg.sender];
+    }
 
     // Deposit function
+    function deposit() public payable {
+        require(msg.value > 0, "Amount must more than 0.");
+        accounts[msg.sender] += msg.value;
+    }
 
     // Withdraw function
+    function withdraw(uint256 money) public {
+        require(money <= accounts[msg.sender], "Balance is not enough.");
+        payable(msg.sender).transfer(money);
+        accounts[msg.sender] -= money;
+    }
 }
