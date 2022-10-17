cf7a0d414cd5e567bae9dfb0bf515a055c0b1b60
Tanin <tanin.tantisirawattanakul@iamconsulting.co.th>
2022-10-17 17:08:37 +0700

    Init commit

---
+++ Contract/CrowdFundingWorkshop.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+contract CrowdFunding{
+    // Declare balance parameter
+
+    // Declare owner parameter and constructor
+
+    // Declare event
+
+    // Donate function
+
+    // Get Fund Function
+}
