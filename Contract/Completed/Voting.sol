397d5107d40207130c51a64ed68bbce9b8703be2
Tanin <tanin.tantisirawattanakul@iamconsulting.co.th>
2022-10-21 08:45:15 +0700

    Add complete version

---
+++ Contract/Completed/Voting.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+contract Voting {
+    // Declare vote parameter
+    mapping (string => uint256) public votes;
+    // Declare owner
+    string [] public title;
+    address public owner;
+    constructor(){
+        owner = msg.sender;
+    }
+
+    // Create title function
+    function createTitle(string memory _title) public {
+        require(owner == msg.sender, "Only owner can create title.");
+        title.push(_title);
+        votes[_title] = 0;
+    }
+
+    // Vote
+    function vote(string memory _title) public {
+        votes[_title] += 1;
+    }
+
+    function checkLength()public view returns(uint){
+        return title.length;
+    }
+
+}
